e4e8631192a4c02fee73331c11e20c9d78482f6b
Halvard Mørstad <jobs@morstad.eu>
2023-03-09 15:28:47 +0000

    Updated example

--- test/steps/hello.ts
+++ test/steps/hello.ts
@@ -1,11 +1,13 @@
 import assert from "node:assert";
 import { When, Then } from "@cucumber/cucumber";
+import { TEST_VAR } from "../veriables";
 
 function sayHello() {
   return "hello";
 }
 
 When("the greeter says hello", function () {
+  console.log(TEST_VAR);
   this.whatIHeard = sayHello();
 });
 
